651f62c7a7d2104db58e2aff9ccb355ac72c6c0f
Sugarblade <William.f.skelton@outlook.com>
2016-06-07 18:39:24 -0700

    Added objects for each store

---
+++ app.js
@@ -0,0 +1,36 @@
+// var myObject = {
+//     0: 'a',
+//     1: 'b',
+//     2: 'c'
+// };
+
+var PikePlaceMarket = {
+	minCust: 14,
+	maxCust: 35,
+	cupsPerCust: 1.2,
+	toGoPerCust: 0.34
+};
+var CapitolHill = {
+	minCust: 12,
+	maxCust: 28,
+	cupsPerCust: 3.2,
+	toGoPerCust: 0.03
+};
+var SeattlePublicLibrary = {
+	minCust: 9,
+	maxCust: 45,
+	cupsPerCust: 2.6,
+	toGoPerCust: 0.02
+};
+var SouthLakeUnion = {
+	minCust: 5,
+	maxCust: 18,
+	cupsPerCust: 1.3,
+	toGoPerCust: 0.04
+};
+var Sea-TacAirport = {
+	minCust: 28,
+	maxCust: 44,
+	cupsPerCust: 1.1,
+	toGoPerCust: 0.41
+};
