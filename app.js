b2d6cb06161fc0ddda1f43de8c5ea58f154dd9ce
Sugarblade <William.f.skelton@outlook.com>
2016-06-07 21:07:14 -0700

    Finally got process working for first object

--- app.js
+++ app.js
@@ -5,34 +5,66 @@ function specificRandom(min, max){
 	return Math.random() * (max-min) + min;
 }
 
-// Fills a store's numCustomers array with a random int between minCust and maxCust
-function generateDalyCustomers(store){
-	for(var i = 0; i < 18; i++){
-		store.numCustomers[i] = Math.floor(specificRandom(store.minCust, store.maxCust));
-	}
-}
+
+
 
 function createSection(){
 	var parent = document.getElementsByTagName("body");
 	var child = document.createElement("section");
-	console.log(parent);
+	console.log(child);
 	parent[0].appendChild(child);
 }
 
-createUL: function(){
-	
-}
+
 
 /* ========== Objects ========== */
 
 // Store Objects
 var PikePlaceMarket = {
+
+	/* ========== Properties ==========*/
 	storeName: "Pike Place Market",
 	minCust: 14,
 	maxCust: 35,
 	cupsPerCust: 1.2,
 	toGoPerCust: 0.34,
+	hours: ["6:00am", "7:00am", "8:00am", "9:00am", "10:00am", "11:00am", "12:00pm", "1:00pm", "2:00pm", "3:00pm", "4:00pm", "5:00pm", "6:00pm", "7:00pm", "8:00pm", "9:00pm"],
 	numCustomers: [],
+	totalCupLBSPerHr: [],
+	totalLBSToGo: [],
+
+/* ========== Methods ==========*/
+// Fills a store's numCustomers array with a random int between minCust and maxCust
+	generateDalyCustomers: function(){
+		for(var i = 0; i < 18; i++){
+			this.numCustomers[i] = Math.floor(specificRandom(this.minCust, this.maxCust));
+			this.totalCupLBSPerHr[i] = this.numCustomers[i] * this.cupsPerCust;
+			this.totalLBSToGo[i] = this.numCustomers[i] * this.toGoPerCust;
+		}
+	},
+	createUL: function(){
+		var parent = document.getElementsByTagName("section");
+		var child = document.createElement("ul");
+		var title = document.createElement("h2");
+		title.textContent = this.storeName;
+		parent[0].appendChild(title);
+		console.log(child);
+		child.id = this.storeName.replace(/ /g, "");
+		console.log(child.id);
+		parent[0].appendChild(child);
+	},
+
+	createLI: function(){
+		var parent = document.getElementsByTagName("ul");
+
+		for(var i in this.hours){
+			var child = document.createElement("li");
+			child.textContent = (this.hours[i] + ": " + ((this.totalCupLBSPerHr[i]) + (this.totalLBSToGo[i])).toFixed(1) + " [" + this.numCustomers[i] + " customers, " + (this.totalCupLBSPerHr[i].toFixed(1)) + " cups (" + ((this.totalCupLBSPerHr[i]/16).toFixed(1)) + " lbs ), "  + (this.totalLBSToGo[i].toFixed(1)) + "lbs to-go]");
+			console.log(child);
+			parent[0].appendChild(child);
+		}
+
+	}
 };
 
 var CapitolHill = {
@@ -42,6 +74,8 @@ var CapitolHill = {
 	cupsPerCust: 3.2,
 	toGoPerCust: 0.03,
 	numCustomers: [],
+
+
 };
 
 var SeattlePublicLibrary = {
@@ -51,6 +85,8 @@ var SeattlePublicLibrary = {
 	cupsPerCust: 2.6,
 	toGoPerCust: 0.02,
 	numCustomers: [],
+
+
 };
 
 var SouthLakeUnion = {
@@ -60,6 +96,8 @@ var SouthLakeUnion = {
 	cupsPerCust: 1.3,
 	toGoPerCust: 0.04,
 	numCustomers: [],
+
+
 };
 
 var SeaTacAirport = {
@@ -69,6 +107,8 @@ var SeaTacAirport = {
 	cupsPerCust: 1.1,
 	toGoPerCust: 0.41,
 	numCustomers: [],
+
+
 };
 
 /* ========== Global Variables ========== */
@@ -84,10 +124,13 @@ var hours = ["6:00", "7:00", "8:00", "9:00", "10:00", "11:00", "12:00", "13:00",
 
 // Calls generateDalyCustomers on all stores
 for(var i = 0; i < allStores.length; i++){
-	generateDalyCustomers(allStores[i]);
+	PikePlaceMarket.generateDalyCustomers();
 }
 
 createSection();
+PikePlaceMarket.createUL();
+PikePlaceMarket.createLI();
+
 // for(var i = 0; i < allStores.length; i++){
 // 	console.log(allStores[i].storeName);
 // 	console.log(allStores[i].numCustomers);
